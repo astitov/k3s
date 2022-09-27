31ee20b5b8241eaab5de8cfe93901dc372b67e24
Titov <98889670+astitov@users.noreply.github.com>
2022-09-27 15:06:56 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,20 +4,46 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.16"
     }
+    random = {
+      source = "hashicorp/random"
+    }
   }
-
   required_version = ">= 1.2.0"
 }
+
+provider "random" {}
 
 provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_instance" "app_server" {
+resource "random_pet" "name" {
+  length = 2
+}
+
+resource "aws_instance" "my-vm" {
   ami           = "ami-07eeacb3005b9beae"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.my-sgroup.id]
 
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = random_pet.name.id
+  }
+}
+
+resource "aws_security_group" "my-sgroup" {
+  name = "${random_pet.name.id]-sgroup"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
