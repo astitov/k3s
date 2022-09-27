d5b3334019e6caf3ba77e30bf76ce7a8a0db475a
Titov <98889670+astitov@users.noreply.github.com>
2022-09-27 16:50:02 +0300

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "pub_cidr" {
+  type    = string
+  default = "10.0.10.0/24"
+}
+
+variable "priv_cidr" {
+  type    = string
+  default = "10.0.20.0/24"
+}
