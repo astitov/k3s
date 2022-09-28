2d4394b984303811ea6b50ad7043db9663b69d5a
Titov <98889670+astitov@users.noreply.github.com>
2022-09-28 14:33:38 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -69,7 +69,7 @@ resource "aws_route_table" "my-natgw-rt" {
   vpc_id = aws_vpc.my-vpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.my-natgw.id
+    gateway_id = aws_nat_gateway.my-natgw.id
   }
 }
 
