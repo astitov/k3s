50989e2944666d699bcaec10db35bf6ca3177840
Titov <98889670+astitov@users.noreply.github.com>
2022-09-29 13:47:21 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,68 +25,68 @@ resource "random_pet" "name" {
 #   VPC and networks
 #
 
-resource "aws_vpc" "my-vpc" {
+resource "aws_vpc" "my_vpc" {
   cidr_block = var.vpc_cidr
 }
 
-resource "aws_subnet" "my-pubnet" {
-  vpc_id                  = aws_vpc.my-vpc.id
+resource "aws_subnet" "my_pubnet" {
+  vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = var.pub_cidr
   map_public_ip_on_launch = true
 }
 
-resource "aws_subnet" "my-privnet" {
-  vpc_id     = aws_vpc.my-vpc.id
+resource "aws_subnet" "my_privnet" {
+  vpc_id     = aws_vpc.my_vpc.id
   cidr_block = var.priv_cidr
 }
 
 # Gateways
 
-resource "aws_internet_gateway" "my-igw" {
-  vpc_id = aws_vpc.my-vpc.id
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
 }
 
-resource "aws_eip" "my-elastic-ip" {
+resource "aws_eip" "my_elastic_ip" {
   vpc = true
 }
 
-resource "aws_nat_gateway" "my-natgw" {
-  allocation_id = aws_eip.my-elastic-ip.id
-  subnet_id     = aws_subnet.my-pubnet.id
+resource "aws_nat_gateway" "my_natgw" {
+  allocation_id = aws_eip.my_elastic_ip.id
+  subnet_id     = aws_subnet.my_pubnet.id
 }
 
 # Routing tables
 
-resource "aws_route_table" "my-igw-rt" {
-  vpc_id = aws_vpc.my-vpc.id
+resource "aws_route_table" "my_igw_rt" {
+  vpc_id = aws_vpc.my_vpc.id
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.my-igw.id
   }
 }
 
-resource "aws_route_table" "my-natgw-rt" {
-  vpc_id = aws_vpc.my-vpc.id
+resource "aws_route_table" "my_natgw_rt" {
+  vpc_id = aws_vpc.my_vpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.my-natgw.id
+    gateway_id = aws_nat_gateway.my_natgw.id
   }
 }
 
-resource "aws_route_table_association" "my-rt-pubnet-assoc" {
-  subnet_id      = aws_subnet.my-pubnet.id
-  route_table_id = aws_route_table.my-igw-rt.id
+resource "aws_route_table_association" "my_rt_pubnet_assoc" {
+  subnet_id      = aws_subnet.my_pubnet.id
+  route_table_id = aws_route_table.my_igw_rt.id
 }
 
-resource "aws_route_table_association" "my-rt-privnet-assoc" {
-  subnet_id      = aws_subnet.my-privnet.id
-  route_table_id = aws_route_table.my-natgw-rt.id
+resource "aws_route_table_association" "my_rt_privnet_assoc" {
+  subnet_id      = aws_subnet.my_privnet.id
+  route_table_id = aws_route_table.my_natgw_rt.id
 }
 
 # Firewall
 
-resource "aws_security_group" "my-pub-sg" {
-  vpc_id = aws_vpc.my-vpc.id
+resource "aws_security_group" "my_pub_sg" {
+  vpc_id = aws_vpc.my_vpc.id
 
   ingress {
     from_port   = 22
@@ -103,8 +103,8 @@ resource "aws_security_group" "my-pub-sg" {
   }
 }
 
-resource "aws_security_group" "my-priv-sg" {
-  vpc_id = aws_vpc.my-vpc.id
+resource "aws_security_group" "my_priv_sg" {
+  vpc_id = aws_vpc.my_vpc.id
 
   ingress {
     from_port   = 80
@@ -116,7 +116,7 @@ resource "aws_security_group" "my-priv-sg" {
   ingress {
     from_port   = 0
     to_port     = 0
-    protocol    = "tcp"
+    protocol    = "-1"
     cidr_blocks = [var.vpc_cidr]
   }
   
@@ -133,17 +133,17 @@ resource "aws_security_group" "my-priv-sg" {
 #   VM instances
 #
 
-resource "aws_instance" "my-vm" {
+resource "aws_instance" "my_master" {
   ami                    = "ami-07eeacb3005b9beae"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.my-pub-sg.id]
-  subnet_id              = aws_subnet.my-pubnet.id
+  vpc_security_group_ids = [aws_security_group.my_pub_sg.id]
+  subnet_id              = aws_subnet.my_pubnet.id
   user_data              = "curl -sfL https://get.k3s.io | sh -"
 }
 
 resource "aws_instance" "my-worker" {
   ami                    = "ami-07eeacb3005b9beae"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.my-priv-sg.id]
-  subnet_id              = aws_subnet.my-privnet.id
+  vpc_security_group_ids = [aws_security_group.my_priv_sg.id]
+  subnet_id              = aws_subnet.my_privnet.id
 }
