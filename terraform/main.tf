47c199f03da6987361555b949cb2b3d562c3e7c7
Titov <98889670+astitov@users.noreply.github.com>
2022-09-29 17:00:26 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -143,7 +143,8 @@ resource "aws_instance" "my_master" {
 
 data "external" "get_k3s_token" {
   depends_on = [aws_instance.my_master]
-  program    = ["cat /var/lib/rancher/k3s/server/node-token"]
+  program    = ["date"]
+  # program    = ["cat /var/lib/rancher/k3s/server/node-token"]
 }
 
 resource "aws_instance" "my-worker" {
