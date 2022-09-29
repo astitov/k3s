b8ee619298d835a618fd244a691f2cfc1e639652
Titov <98889670+astitov@users.noreply.github.com>
2022-09-29 16:38:49 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -138,7 +138,7 @@ resource "aws_instance" "my_master" {
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.my_pub_sg.id]
   subnet_id              = aws_subnet.my_pubnet.id
-  user_data              = "curl -sfL https://get.k3s.io | sh -
+  user_data              = "curl -sfL https://get.k3s.io | sh -"
 }
 
 data "external" "get_k3s_token" {
