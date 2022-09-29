2565e3901c45ed05516f4cb9ebc7271290d67c4d
Titov <98889670+astitov@users.noreply.github.com>
2022-09-29 13:53:47 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -61,7 +61,7 @@ resource "aws_route_table" "my_igw_rt" {
   vpc_id = aws_vpc.my_vpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.my-igw.id
+    gateway_id = aws_internet_gateway.my_igw.id
   }
 }
 
